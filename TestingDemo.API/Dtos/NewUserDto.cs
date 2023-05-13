87c060db881d1e5edcaa0f70b934ad4323ac8e2d
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-05-13 17:33:17 -0500

    fix: add additional attributes to new user dto

--- TestingDemo.API/Dtos/NewUserDto.cs
+++ TestingDemo.API/Dtos/NewUserDto.cs
@@ -3,11 +3,14 @@ namespace TestingDemo.API.Dtos;
 public class NewUserDto
 {
   [Required]
+  [MaxLength(150)]
   public string Username { get; set; } = string.Empty;
 
   [Required]
+  [EmailAddress]
   public string Email { get; set; } = string.Empty;
 
   [Required]
+  [MinLength(8)]
   public string Password { get; set; } = string.Empty;
 }
