85c4c173847c6ea10101567d364371dd5fd0f42e
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-05-13 17:39:51 -0500

    fix: add password complexity validation

--- TestingDemo.API/Dtos/NewUserDto.cs
+++ TestingDemo.API/Dtos/NewUserDto.cs
@@ -2,6 +2,14 @@ namespace TestingDemo.API.Dtos;
 
 public class NewUserDto
 {
+  // At least one uppercase letter ((?=.*[A-Z]))
+  // At least one lowercase letter ((?=.*[a-z]))
+  // At least one number ((?=.*\d))
+  // At least one special character ((?=.*\W))
+  // Must be at least 8 characters long ({8,})
+  private const string PasswordRegex = @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*\W)[A-Za-z\d\W]{8,}$";
+  private const string PasswordErrorMessage = "The password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character.";
+
   [Required]
   [MaxLength(150)]
   public string Username { get; set; } = string.Empty;
@@ -11,6 +19,6 @@ public class NewUserDto
   public string Email { get; set; } = string.Empty;
 
   [Required]
-  [MinLength(8)]
+  [RegularExpression(PasswordRegex, ErrorMessage = PasswordErrorMessage)]
   public string Password { get; set; } = string.Empty;
 }
