87cabb856f20e6b3c78557739afc04d0b997304c
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-05-14 17:36:25 -0500

    fix: add default values for the token settings model

--- TestingDemo.API/Models.cs/TokenSettings.cs
+++ TestingDemo.API/Models.cs/TokenSettings.cs
@@ -2,7 +2,7 @@ namespace TestingDemo.API.Models;
 
 public class TokenSettings
 {
-  public string JwtKey { get; set; }
-  public string JwtIssuer { get; set; }
-  public string JwtAudience { get; set; }
+  public string JwtKey { get; set; } = string.Empty;
+  public string JwtIssuer { get; set; } = string.Empty;
+  public string JwtAudience { get; set; } = string.Empty;
 }
