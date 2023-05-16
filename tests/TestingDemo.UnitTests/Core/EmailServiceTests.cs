8370feb57a22cdc1a7751eb32dccde031a124657
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-05-15 22:01:54 -0500

    test: write unit test for email service

---
+++ tests/TestingDemo.UnitTests/Core/EmailServiceTests.cs
@@ -0,0 +1,45 @@
+namespace TestingDemo.UnitTests.Core;
+
+public class EmailServiceTests
+{
+  private readonly Mock<IEmailProvider> _emailProviderMock;
+  private readonly EmailSettings _emailSettings;
+  private readonly EmailService _emailService;
+  public EmailServiceTests()
+  {
+    _emailProviderMock = new Mock<IEmailProvider>();
+    _emailSettings = new EmailSettings
+    {
+      SystemEmailAddress = "testing@test.com"
+    };
+
+    _emailService = new EmailService(
+      _emailProviderMock.Object,
+      _emailSettings
+    );
+  }
+
+  [Fact]
+  public async Task SendPasswordResetEmail_WhenCalled_ShouldSendPasswordResetEmailWithTokenToEmailGiven()
+  {
+    var recipient = "recipient";
+    var token = "token";
+    var expectedSubject = "Password Reset";
+
+    await _emailService.SendPasswordResetEmail(recipient, token);
+
+    _emailProviderMock.Verify(
+      x => x.SendEmail(
+        It.Is<EmailMessage>(
+          m =>
+            m.Sender == _emailSettings.SystemEmailAddress &&
+            m.Recipient == recipient &&
+            m.Subject == expectedSubject &&
+            m.TextContent.Contains(token) &&
+            m.HtmlContent.Contains(token)
+        )
+      ),
+      Times.Once()
+    );
+  }
+}
--- tests/TestingDemo.UnitTests/Usings.cs
+++ tests/TestingDemo.UnitTests/Usings.cs
@@ -1,1 +1,8 @@
+global using Moq;
+
+global using TestingDemo.Core.Interfaces;
+global using TestingDemo.Core.Models;
+
+global using TestingDemo.Core.Services;
+
 global using Xunit;
