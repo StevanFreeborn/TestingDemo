c70d7dedd81020f62002061bbcac010b002ab3f5
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-05-19 01:07:36 -0500

    feat: server react up in dev using https

--- src/TestingDemo.Client/src/App.tsx
+++ src/TestingDemo.Client/src/App.tsx
@@ -2,10 +2,13 @@ import { MouseEvent, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UserActions, useUserContext } from './context/UserContext';
 import { useAuthService } from './services/authService';
+import { useClient } from './services/httpClient';
 
 function App() {
   const { state, dispatch } = useUserContext();
   const { refreshToken } = useAuthService();
+  const client = useClient();
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -21,11 +24,22 @@ function App() {
     navigate('/Public/Login');
   }
 
+  async function makeRequest() {
+    const res = await client.get(
+      'https://localhost:5000/api/auth/reset-password'
+    );
+
+    console.log(res);
+  }
+
   return (
     <div>
       <h1>Hello World</h1>
       <button type="button" onClick={handleLogOutClick}>
         Log Out
+      </button>
+      <button type="button" onClick={() => makeRequest()}>
+        Make Request
       </button>
     </div>
   );
