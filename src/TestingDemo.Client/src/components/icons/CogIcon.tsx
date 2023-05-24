a380e318278a22cefa2ca6fc96deb195e0ec24cb
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-05-24 09:54:08 -0500

    chore: add cog icon

---
+++ src/TestingDemo.Client/src/components/icons/CogIcon.tsx
@@ -0,0 +1,5 @@
+import { ReactComponent as CogSvg } from '../../assets/images/icons/cog.svg';
+
+export default function CogIcon({ className }: { className: string }) {
+  return <CogSvg className={className} />;
+}
