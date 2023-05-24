e12bc5247c6a6b03324c75f7b4c97fc210444b3e
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-05-24 09:57:09 -0500

    chore: add help and arrow icons

---
+++ src/TestingDemo.Client/src/components/icons/HelpCircleIcon.tsx
@@ -0,0 +1,5 @@
+import { ReactComponent as HelpCircleSvg } from '../../assets/images/icons/help-circle-outline.svg';
+
+export default function HelpCircleIcon({ className }: { className: string }) {
+  return <HelpCircleSvg className={className} />;
+}
---
+++ src/TestingDemo.Client/src/components/icons/LeftArrowIcon.tsx
@@ -0,0 +1,5 @@
+import { ReactComponent as LeftArrowSvg } from '../../assets/images/icons/arrow-left-bold.svg';
+
+export default function LeftArrowIcon({ className }: { className: string }) {
+  return <LeftArrowSvg className={className} />;
+}
