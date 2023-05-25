fb971d5d3ca39f67115c639204f2f10ebf582346
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-05-24 19:24:39 -0500

    tests: begin writing tests for UserService class. fix: inject authenticator instead of using static class and methods

--- tests/TestingDemo.UnitTests/Core/Services/UserServiceTests.cs
+++ tests/TestingDemo.UnitTests/Core/Services/UserServiceTests.cs
@@ -3,11 +3,86 @@ namespace TestingDemo.UnitTests.Core.Services;
 public class UserServiceTests
 {
   private readonly Mock<IUserRepository> _userRepositoryMock;
+  private readonly Mock<IAuthenticator> _authenticatorMock;
+
   private readonly UserService _userService;
 
   public UserServiceTests()
   {
     _userRepositoryMock = new Mock<IUserRepository>();
-    _userService = new UserService(_userRepositoryMock.Object);
+    _authenticatorMock = new Mock<IAuthenticator>();
+    _userService = new UserService(
+      _userRepositoryMock.Object,
+      _authenticatorMock.Object
+    );
+  }
+
+  [Fact]
+  public async Task GetUserByIdAsync_WhenCalledAndUserIsNotFound_ItShouldThrowAModelNotFoundException()
+  {
+    _userRepositoryMock.Setup(
+      m => m.GetByIdAsync(It.IsAny<string>())
+    )
+    .ReturnsAsync((User?) null);
+
+    var action = () => _userService.GetUserByIdAsync("id");
+
+    await action.Should().ThrowAsync<ModelNotFoundException>();
+  }
+
+  [Fact]
+  public async Task GetUserByIdAsync_WhenCalledAndUserIsFound_ItShouldReturnTheUser()
+  {
+    var user = new User();
+
+    _userRepositoryMock.Setup(
+      m => m.GetByIdAsync(It.IsAny<string>())
+    )
+    .ReturnsAsync(user);
+
+    var result = await _userService.GetUserByIdAsync(user.Id);
+
+    result.Should().NotBeNull();
+    result.Should().Be(user);
+  }
+
+  [Fact]
+  public async Task LogUserInAsync_WhenCalledAndUserIsNotFound_ItShouldThrowAnInvalidLoginException()
+  {
+    _userRepositoryMock.Setup(
+      m => m.GetByIdAsync(It.IsAny<string>())
+    )
+    .ReturnsAsync((User?) null);
+
+    var action = () => _userService.LogUserInAsync("username", "password");
+
+    await action.Should().ThrowAsync<InvalidLoginException>();
+  }
+
+  [Fact]
+  public async Task LogUserInAsync_WhenCalledUserIsFoundButPasswordIsIncorrect_ItShouldThrowAnInvalidLoginException()
+  {
+    var user = new User();
+
+    _userRepositoryMock.Setup(
+      m => m.GetByIdAsync(It.IsAny<string>())
+    )
+    .ReturnsAsync(user);
+
+    _authenticatorMock.Setup(
+      m => m.VerifyPassword(
+        It.IsAny<string>(),
+        It.IsAny<string>(),
+        It.IsAny<string>()
+      )
+    )
+    .Returns(false);
+
+    var action = () => _userService.LogUserInAsync(
+      user.Username,
+      user.Password
+    );
+
+    await action.Should().ThrowAsync<InvalidLoginException>();
   }
 }
--- tests/TestingDemo.UnitTests/Usings.cs
+++ tests/TestingDemo.UnitTests/Usings.cs
@@ -1,8 +1,8 @@
 global using FluentAssertions;
-global using FluentAssertions.Extensions;
 
 global using Moq;
 
+global using TestingDemo.Core.Exceptions;
 global using TestingDemo.Core.Interfaces;
 global using TestingDemo.Core.Models;
 global using TestingDemo.Core.Services;
