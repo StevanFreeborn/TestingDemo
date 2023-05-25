fd5af48487d2895f35f239dfb3fbc3dda77fd55b
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-05-25 14:52:14 -0500

    feat: add search modal for sidebar when collapsed

--- src/TestingDemo.Client/src/components/Sidebar.tsx
+++ src/TestingDemo.Client/src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, useState } from 'react';
+import { MouseEvent, useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import defaultProfilePicture from '../assets/images/default-profile-picture.png';
 import logo from '../assets/images/testing-demo-logo-180-47.svg';
@@ -12,7 +12,7 @@ import MagnifyIcon from './icons/MagnifyIcon';
 import ReportIcon from './icons/ReportIcon';
 import RightArrowIcon from './icons/RightArrowIcon';
 
-function SideBarHeader() {
+function SideBarHeader({ isCollapsed }: { isCollapsed: boolean }) {
   const username = 'Stevan';
   const role = 'System Administrator';
   return (
@@ -22,14 +22,17 @@ function SideBarHeader() {
           <img alt="testing-demo-logo" src={logo} className={styles.logo} />
         </Link>
       </div>
+
       <div className={styles.profileContainer}>
         <div className={styles.profilePictureContainer}>
           <img alt="default-profile" src={defaultProfilePicture} />
         </div>
-        <div className={styles.profileInfoContainer}>
-          <div className={styles.userName}>{username}</div>
-          <div className={styles.currentRole}>{role}</div>
-        </div>
+        {isCollapsed ? null : (
+          <div className={styles.profileInfoContainer}>
+            <div className={styles.userName}>{username}</div>
+            <div className={styles.currentRole}>{role}</div>
+          </div>
+        )}
       </div>
     </div>
   );
@@ -80,6 +83,106 @@ function SideBarFooter({
   );
 }
 
+function SearchInput({ containerPadding }: { containerPadding?: number }) {
+  return (
+    <div
+      style={{ padding: containerPadding }}
+      className={styles.searchContainer}
+    >
+      <input type="text" placeholder="Search All Content"></input>
+      <div className={styles.searchIconContainer}>
+        <MagnifyIcon className={styles.searchIcon} />
+      </div>
+    </div>
+  );
+}
+
+function SearchModalButton() {
+  const [isInputDisplayed, setIsInputDisplayed] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(e: globalThis.MouseEvent) {
+      if (
+        containerRef.current &&
+        containerRef.current.contains(e.target as Node) === false
+      ) {
+        setIsInputDisplayed(false);
+      }
+    }
+
+    document.addEventListener('click', handleClickOutside);
+    return () => document.removeEventListener('click', handleClickOutside);
+  }, [containerRef, setIsInputDisplayed, isInputDisplayed]);
+
+  function handleButtonClick(e: MouseEvent<HTMLButtonElement>) {
+    setIsInputDisplayed(!isInputDisplayed);
+  }
+
+  return (
+    <div ref={containerRef} className={styles.searchContainerCollapsed}>
+      <button
+        onClick={handleButtonClick}
+        className={styles.searchIconContainerCollapsed}
+      >
+        <MagnifyIcon className={styles.searchIcon} />
+      </button>
+      <div
+        className={
+          isInputDisplayed ? styles.searchSliderSlide : styles.searchSlider
+        }
+      >
+        <SearchInput containerPadding={0} />
+      </div>
+    </div>
+  );
+}
+
+function SideBarSearch({ isCollapsed }: { isCollapsed: boolean }) {
+  return isCollapsed ? <SearchModalButton /> : <SearchInput />;
+}
+
+function SideBarBody({ isCollapsed }: { isCollapsed: boolean }) {
+  return (
+    <div className={styles.navBodyContainer}>
+      <div className={styles.navigationTopContainer}>
+        <SideBarSearch isCollapsed={isCollapsed} />
+        <div title="Dashboards" className={styles.navItemActive}>
+          <div className={styles.navItemIconContainer}>
+            <GaugeIcon className={styles.navItemIcon} />
+          </div>
+          <div className={styles.navLink}>Dashboards</div>
+        </div>
+        <Link title="Reports" to="/Report" className={styles.navLink}>
+          <div className={styles.navItem}>
+            <div className={styles.navItemIconContainer}>
+              <ReportIcon className={styles.navItemIcon} />
+            </div>
+            <div className={styles.navLink}>Reports</div>
+          </div>
+        </Link>
+        <Link title="Content" to="/Content" className={styles.navLink}>
+          <div className={styles.navItem}>
+            <div className={styles.navItemIconContainer}>
+              <ContentIcon className={styles.navItemIcon} />
+            </div>
+            <div className={styles.navLink}>Content</div>
+          </div>
+        </Link>
+      </div>
+      <div className={styles.navigationBottomContainer}>
+        <Link title="Administration" to="/Admin" className={styles.navLink}>
+          <div className={styles.navItem}>
+            <div className={styles.gearIconContainer}>
+              <CogIcon className={styles.gearIcon} />
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
 export default function SideBar() {
   const [isCollapsed, setIsCollapsed] = useState(false);
 
@@ -91,48 +194,8 @@ export default function SideBar() {
     <nav
       className={isCollapsed ? styles.primaryNavCollapsed : styles.primaryNav}
     >
-      <SideBarHeader />
-      <div className={styles.navBodyContainer}>
-        <div className={styles.navigationTopContainer}>
-          <div className={styles.searchContainer}>
-            <input type="text" placeholder="Search All Content"></input>
-            <div className={styles.searchIconContainer}>
-              <MagnifyIcon className={styles.searchIcon} />
-            </div>
-          </div>
-          <div title="Dashboards" className={styles.navItemActive}>
-            <div className={styles.navItemIconContainer}>
-              <GaugeIcon className={styles.navItemIcon} />
-            </div>
-            <div className={styles.navLink}>Dashboards</div>
-          </div>
-          <Link title="Reports" to="/Report" className={styles.navLink}>
-            <div className={styles.navItem}>
-              <div className={styles.navItemIconContainer}>
-                <ReportIcon className={styles.navItemIcon} />
-              </div>
-              <div className={styles.navLink}>Reports</div>
-            </div>
-          </Link>
-          <Link title="Content" to="/Content" className={styles.navLink}>
-            <div className={styles.navItem}>
-              <div className={styles.navItemIconContainer}>
-                <ContentIcon className={styles.navItemIcon} />
-              </div>
-              <div className={styles.navLink}>Content</div>
-            </div>
-          </Link>
-        </div>
-        <div className={styles.navigationBottomContainer}>
-          <Link title="Administration" to="/Admin" className={styles.navLink}>
-            <div className={styles.navItem}>
-              <div className={styles.gearIconContainer}>
-                <CogIcon className={styles.gearIcon} />
-              </div>
-            </div>
-          </Link>
-        </div>
-      </div>
+      <SideBarHeader isCollapsed={isCollapsed} />
+      <SideBarBody isCollapsed={isCollapsed} />
       <SideBarFooter
         isCollapsed={isCollapsed}
         arrowButtonClickHandler={handleCollapseButtonClick}
